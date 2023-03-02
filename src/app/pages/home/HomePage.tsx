8363f87da0050e11f030dd9ed893817ecfc6fcb1
Дмитрий Алексеев <d.alekseev@uplab.ru>
2023-03-02 20:13:01 +0300

    home

--- src/app/pages/home/HomePage.tsx
+++ src/app/pages/home/HomePage.tsx
@@ -13,7 +13,6 @@ export const HomePage = () => {
   if (data) {
     return (
       <>
-  
         <Slider />
         <Container maxWidth="lg">
           <IconLinkList links={data.iconLinkList} />
